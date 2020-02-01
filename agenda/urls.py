9b0838bbaea6fb69535a59ee94836f8908cf13a1
Cesar Augusto de Moraes Costa <cesar@localhost.localdomain>
2020-02-01 03:57:29 -0300

    Incluido função e tela de autenticação.

--- agenda/urls.py
+++ agenda/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from core import views
@@ -22,5 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
     #path('', views.index), #com a função em views
-    path ('', RedirectView.as_view(url='/agenda'))
+    path ('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
